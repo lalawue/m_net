f4053bd641baa6cd76a9f0e22105cd54f2d1d4ea
lalawue <suchaaa@gmail.com>
2020-01-25 00:18:23 +0800

    rename lib under windows

--- MinGW64Make.bat
+++ MinGW64Make.bat
@@ -1,4 +1,4 @@
 mkdir build
-gcc -O2 -Isrc src\mnet_core.c -lws2_32 -shared -fPIC -o build\libmnet.dll
+gcc -O2 -Isrc src\mnet_core.c -lws2_32 -shared -fPIC -o build\mnet.dll
 gcc -O2 -Isrc src\mnet_core.c examples\echo_cnt.cpp -lws2_32 -DEXAMPLE_ECHO_CNT -o build\echo_cnt.exe
 g++ -O2 -Isrc src\mnet_core.c examples\echo_svr.cpp -lws2_32 -DEXAMPLE_ECHO_SVR -o build\echo_svr.exe
