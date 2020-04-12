c0ef19d9cc9c0b862e869e79514284bad8ccb139
lalawue <suchaaa@gmail.com>
2020-04-12 10:25:52 +0800

    better echo server pull style

--- examples/echo_svr_pull_style.c
+++ examples/echo_svr_pull_style.c
@@ -27,41 +27,35 @@ int main(int argc, char *argv[]) {
    mnet_chann_listen(svr, addr.ip, addr.port, 100);
    printf("svr start listen: %s\n", ipaddr);
 
-   while (1) {
+   for (;;) {
       results = mnet_poll(100000);
       if (results->chann_count <= 0) {
          break;
       }
       chann_msg_t *msg = NULL;
       while ((msg = mnet_result_next(results))) {
-         switch (msg->event) {
-            case CHANN_EVENT_ACCEPT: {
-               if (msg->n == svr) {
-                  /* sever accept client */
-                  chann_addr_t addr;
-                  char welcome[] = "Welcome to echoServ\n";
-                  mnet_chann_send(msg->r, welcome, sizeof(welcome));
-                  mnet_chann_addr(msg->r, &addr);
-                  printf("svr accept cnt with chann %s:%d\n", addr.ip, addr.port);
-               }
-               break;
-            }
-            case CHANN_EVENT_RECV: {
-               /* only recv one message then disconnect */
-               rw_result_t *rw = mnet_chann_recv(msg->n, buf, 256);
-               mnet_chann_send(msg->n, buf, rw->ret);
-               break;
-            }
-            case CHANN_EVENT_DISCONNECT: {
+         if (msg->n == svr) {
+            if (msg->event == CHANN_EVENT_ACCEPT) {
+               /* sever accept client */
                chann_addr_t addr;
-               mnet_chann_addr(msg->n, &addr);
-               printf("svr disconnect cnt with chann %s:%d\n", addr.ip, addr.port);
-               mnet_chann_close(msg->n);
-               break;
+               char welcome[] = "Welcome to echoServ\n";
+               mnet_chann_send(msg->r, welcome, sizeof(welcome));
+               mnet_chann_addr(msg->r, &addr);
+               printf("svr accept cnt with chann %s:%d\n", addr.ip, addr.port);
             }
-            default: {
-               break;
-            }
+            continue;
+         }
+         /* client event */
+         if (msg->event == CHANN_EVENT_RECV) {
+            /* only recv one message then disconnect */
+            rw_result_t *rw = mnet_chann_recv(msg->n, buf, 256);
+            mnet_chann_send(msg->n, buf, rw->ret);
+         }
+         if (msg->event == CHANN_EVENT_DISCONNECT) {
+            chann_addr_t addr;
+            mnet_chann_addr(msg->n, &addr);
+            printf("svr disconnect cnt with chann %s:%d\n", addr.ip, addr.port);
+            mnet_chann_close(msg->n);
          }
       }
    }
